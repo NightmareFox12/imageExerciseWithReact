173648f3967528a77898bbcb2b0efedf601ddd12
NightmareFox <carloshenriquez392004@gmail.com>
2024-05-30 14:04:57 -0400

    commit example

--- src/api/randomUser.js
+++ src/api/randomUser.js
@@ -1,6 +1,6 @@
 export const getImages = async number => {
   const req = await fetch(`https://randomuser.me/api/?results=${number}`);
   const res = await req.json();
-
+  console.log(req)
   return res.results;
 }
